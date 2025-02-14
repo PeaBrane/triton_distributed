904cd3b5b0d7284e3f3de8d6f2f2152cb8619a81
idhanani <idhanani@nvidia.com>
2025-02-13 23:53:05 -0800

    works -- no streaming

--- examples/python_rs/llm/vllm/disaggregated/nova/decode.py
+++ examples/python_rs/llm/vllm/disaggregated/nova/decode.py
@@ -1,14 +1,17 @@
 import uuid
 
 import vllm
-from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.arg_utils import AsyncEngineArgs, KVTransferConfig
 from vllm.logger import logger as vllm_logger
 
 # from nova_init.decorators import nova_endpoint, nova_service, nova_depends
+import os
 from prefill import Prefill
 from compoundai import depends, nova_endpoint, service, api
+import torch
 
-from huggingface_hub import snapshot_download
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.utils import FlexibleArgumentParser
 
 @service(
     nova={
@@ -20,25 +23,25 @@ class Decode:
     prefill = depends(Prefill)
 
     def __init__(self):
-        print("booting up decoding")
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+        os.environ["NCCL_DEBUG"] = "WARN"
         engine_args = AsyncEngineArgs(
             model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
             max_model_len=100,
             gpu_memory_utilization=0.8,
             enforce_eager=True,
-            kv_transfer_config={
-                "kv_connector": "PyNcclConnector",
-                "kv_role": "kv_consumer",
-                "kv_rank": 1,
-                "kv_parallel_size": 2
-            }
-        )
-        # TODO: hacked right now
+            kv_transfer_config=KVTransferConfig(
+                kv_connector="PyNcclConnector",
+                kv_role="kv_consumer",
+                kv_rank=1,
+                kv_parallel_size=2
+            ),
+        ) 
         assert (
-            engine_args.kv_transfer_config["kv_role"] == "kv_consumer"
+            engine_args.kv_transfer_config.kv_role == "kv_consumer"
         ), "Decode worker must be a KV consumer"
         self.engine = vllm.AsyncLLMEngine.from_engine_args(engine_args)
-        print("healthcheck", self.engine.check_health())
+        print(self.engine.__repr__())
 
     @nova_endpoint # should have req/resp types
     async def generate(self, request):
